11c1f92f13200f0d86d532d306df631cb30eefd7
Ilya <peshenator@yahoo.com>
2023-05-07 13:34:17 +0200

    Unified names of the variables with other Stefan codes and lecture notes

--- codes/Stefan1D_FTCS/Energy.m
+++ codes/Stefan1D_FTCS/Energy.m
@@ -1,11 +1,11 @@
 function Q = Energy(T)
 
-global Tc hs rhoS rhoL CS CL;
+global Tc hL rhoS rhoL CS CL;
 
 if (T < Tc)
     Q = rhoS*CS*(T-Tc);
 else
-    Q = rhoL*CL*(T-Tc) + rhoL*hs;
+    Q = rhoL*CL*(T-Tc) + rhoL*hL;
 end
 
 
--- codes/Stefan1D_FTCS/Temperature.m
+++ codes/Stefan1D_FTCS/Temperature.m
@@ -1,13 +1,13 @@
 function T = Temperature(Q)
 
-global rhoS rhoL CS CL Tc hs;
+global rhoS rhoL CS CL Tc hL;
 
 if (Q < 0)
     % ice 
     T = (Q + rhoS*CS*Tc)/(rhoS*CS);
-elseif (Q > rhoL*hs)
+elseif (Q > rhoL*hL)
     % water
-    T = (Q - rhoL*hs+rhoL*CL*Tc)/(rhoL*CL);
+    T = (Q - rhoL*hL+rhoL*CL*Tc)/(rhoL*CL);
 else
     T = Tc;
 end
